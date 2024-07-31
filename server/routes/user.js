ab8a6eaa548e743a66c12e7c74d7de93704c6f35
Cryptid <09akshatmishra@gmail.com>
2024-07-31 21:56:42 +0530

    Upload

--- server/routes/user.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { requireAuth } = require('../middleware/auth');
-
-router.get('/profile', requireAuth, (req, res) => {
-  res.json({ user: req.user });
-});
-
-module.exports = router;
